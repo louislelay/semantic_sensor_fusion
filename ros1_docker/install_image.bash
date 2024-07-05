5fbab52d74dab1d8706280c0fd209882443fda6e
Louis LE LAY <le.lay.louis@gmail.com>
2024-07-05 18:08:16 +0200

    rearranging to keep only the pkg

--- ros1_docker/install_image.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build dockerfile
-docker build --tag ros1_docker \
---build-arg USER_ID=$(id -u) \
---build-arg GROUP_ID=$(id -g) .
-docker image prune -f
-
--- ros1_docker/setup.bash
+++
@@ -1,9 +0,0 @@
-# xauth
-xauth merge /dot.Xauthority
-
-# ROS
-source /opt/ros/kinetic/setup.bash
-
-# LiDAR setup
-sudo ifconfig enp2s0f2 192.168.3.100
-sudo route add 192.168.1.201 enp2s0f2
