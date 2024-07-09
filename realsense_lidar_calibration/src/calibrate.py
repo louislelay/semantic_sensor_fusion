9ed95337c02d0c2fc18cddd93cc7428c783b8b26
Louis LE LAY <le.lay.louis@gmail.com>
2024-07-09 06:30:49 +0200

    up

--- realsense_lidar_calibration/src/calibrate.py
+++ realsense_lidar_calibration/src/calibrate.py
@@ -1,118 +1,82 @@
+#!/usr/bin/env python
+
 import rospy
-import yaml
-import os
-import tf
-import pcl
-import numpy as np
+import message_filters
 from sensor_msgs.msg import PointCloud2, Image
 from sensor_msgs import point_cloud2
-import message_filters
-from cv_bridge import CvBridge
+import numpy as np
+import tf
+from cv_bridge import CvBridge, CvBridgeError
 import cv2
 
-# Function to load parameters from YAML file
-def load_params():
-    config_file = rospy.get_param("~config_file")
-    if not os.path.isfile(config_file):
-        rospy.logerr("Config file not found: {}".format(config_file))
-        return None
+class CameraLidarFusion:
+    def __init__(self):
+        rospy.init_node('camera_lidar_fusion', anonymous=True)
 
-    with open(config_file, 'r') as file:
+        self.bridge = CvBridge()
+        self.tf_listener = tf.TransformListener()
+
+        # Subscribers
+        self.image_sub = message_filters.Subscriber('/camera/color/image_raw', Image)
+        self.lidar_sub = message_filters.Subscriber('/velodyne_points', PointCloud2)
+
+        # Synchronize topics
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.image_sub, self.lidar_sub], 10, 0.1)
+        self.ts.registerCallback(self.callback)
+
+        # Publisher
+        self.pub = rospy.Publisher('/camera_lidar_fusion', PointCloud2, queue_size=10)
+
+        # Camera intrinsic parameters (example values, you need to use your calibrated values)
+        self.camera_matrix = np.array([[616.36529541, 0., 320.25881958],
+                                       [0., 616.20294189, 240.0499115],
+                                       [0., 0., 1.]])
+        self.dist_coeffs = np.array([0., 0., 0., 0., 0.])
+
+        # Extrinsic parameters between camera and LiDAR (example values)
+        self.translation = np.array([0.1, 0.0, 0.0])  # Translation vector
+        self.rotation = np.eye(3)  # Rotation matrix
+
+    def callback(self, image_msg, lidar_msg):
         try:
-            params = yaml.safe_load(file)
-        except yaml.YAMLError as exc:
-            rospy.logerr("Error reading config file: {}".format(exc))
-            return None
+            # Convert ROS Image message to OpenCV image
+            cv_image = self.bridge.imgmsg_to_cv2(image_msg, "bgr8")
 
-    return params
+            # Transform LiDAR points to the camera frame
+            points = self.transform_lidar_points(lidar_msg)
 
-# Function to convert ROS PointCloud2 to PCL point cloud
-def ros_to_pcl(ros_cloud):
-    points_list = []
+            # Project LiDAR points to the image plane
+            image_points = self.project_points_to_image(points)
 
-    for data in point_cloud2.read_points(ros_cloud, skip_nans=True):
-        points_list.append([data[0], data[1], data[2]])
+            # Overlay points on the image
+            for pt in image_points:
+                cv2.circle(cv_image, (int(pt[0]), int(pt[1])), 2, (0, 255, 0), -1)
 
-    pcl_data = pcl.PointCloud()
-    pcl_data.from_list(points_list)
+            # Convert OpenCV image back to ROS Image message and publish
+            self.pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
 
-    return pcl_data
+        except CvBridgeError as e:
+            rospy.logerr(e)
 
-# Function to apply voxel grid filtering
-def voxel_grid_filter(pcl_data, leaf_size):
-    voxel_filter = pcl_data.make_voxel_grid_filter()
-    voxel_filter.set_leaf_size(leaf_size, leaf_size, leaf_size)
-    return voxel_filter.filter()
+    def transform_lidar_points(self, lidar_msg):
+        # Convert PointCloud2 message to numpy array
+        points_list = []
+        for point in point_cloud2.read_points(lidar_msg, skip_nans=True):
+            points_list.append([point[0], point[1], point[2]])
+        points = np.array(points_list)
 
-# Function to transform point cloud based on calibration parameters
-def transform_point_cloud(pcl_data, transform_matrix):
-    points = np.array(pcl_data)
-    points_homogeneous = np.hstack((points, np.ones((points.shape[0], 1))))
-    transformed_points = transform_matrix.dot(points_homogeneous.T).T
-    transformed_pcl = pcl.PointCloud()
-    transformed_pcl.from_array(transformed_points[:, :3].astype(np.float32))
-    return transformed_pcl
+        # Apply transformation (extrinsic calibration)
+        points = np.dot(self.rotation, points.T).T + self.translation
+        return points
 
-# Callback function for synchronized data
-def callback(lidar_msg, camera_msg):
-    bridge = CvBridge()
-    
-    # Convert ROS Image to OpenCV image
-    camera_image = bridge.imgmsg_to_cv2(camera_msg, desired_encoding="passthrough")
-
-    # Convert ROS PointCloud2 to PCL
-    lidar_pcl = ros_to_pcl(lidar_msg)
-
-    # Load parameters from YAML file
-    params = load_params()
-    if params is None:
-        rospy.logerr("Failed to load parameters.")
-        return
-    
-    # Extract calibration parameters
-    translation = params['extrinsics']['translation']
-    rotation = params['extrinsics']['rotation']
-    leaf_size = params['point_cloud_filtering']['voxel_leaf_size']
-    
-    # Create transformation matrix
-    translation_vector = np.array([translation['x'], translation['y'], translation['z']])
-    rotation_vector = np.array([rotation['roll'], rotation['pitch'], rotation['yaw']])
-    transform_matrix = tf.transformations.compose_matrix(translate=translation_vector,
-                                                         angles=rotation_vector)
-
-    # Filter point cloud
-    filtered_pcl = voxel_grid_filter(lidar_pcl, leaf_size)
-
-    # Transform point cloud
-    transformed_pcl = transform_point_cloud(filtered_pcl.to_array(), transform_matrix)
-
-    # Visualization
-    if params['visualization']['show_point_cloud']:
-        pcl.visualization.CloudViewing().ShowMonochromeCloud(transformed_pcl)
-
-    if params['visualization']['show_camera_image']:
-        cv2.imshow("Camera Image", camera_image)
-        cv2.waitKey(1)
-
-def calibrate():
-    rospy.init_node('realsense_lidar_calibration', anonymous=True)
-    
-    # Subscribers
-    lidar_sub = message_filters.Subscriber('/lidar_points', PointCloud2)
-    camera_sub = message_filters.Subscriber('/camera/depth/image_raw', Image)
-
-    # Synchronize messages
-    ts = message_filters.ApproximateTimeSynchronizer(
-        [lidar_sub, camera_sub],
-        rospy.get_param('~synchronization/queue_size', 10),
-        rospy.get_param('~synchronization/slop', 0.1)
-    )
-    ts.registerCallback(callback)
-
-    rospy.spin()
+    def project_points_to_image(self, points):
+        # Project 3D points to 2D image plane
+        image_points, _ = cv2.projectPoints(points, np.zeros(3), np.zeros(3), self.camera_matrix, self.dist_coeffs)
+        return image_points.reshape(-1, 2)
 
 if __name__ == '__main__':
     try:
-        calibrate()
+        CameraLidarFusion()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
